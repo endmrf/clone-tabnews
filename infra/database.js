89e913a1828df29faec84cb57d5c3a6348ba7df5
Endryo Moreira Rodrigues Feitosa <endryofeitosa@gmail.com>
2025-02-12 01:03:21 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -9,7 +9,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV == "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -22,6 +22,16 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
